366b08954543d456b933b339daa624ee63cf93ec
asands94 <ashley.sands01@gmail.com>
2022-07-07 04:37:45 -0500

    wip: moved toy model above cat model

--- main_app/models.py
+++ main_app/models.py
@@ -10,6 +10,16 @@ MEALS = (
 )
 
 # Create your models here.
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return f'{self.color} {self.name}'
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+    
 class Cat(models.Model):
   name= models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -45,15 +55,7 @@ class Feeding(models.Model):
   class Meta:
     ordering = ['-date']
 
-class Toy(models.Model):
-  name = models.CharField(max_length=50)
-  color = models.CharField(max_length=20)
 
-  def __str__(self):
-    return f'{self.color} {self.name}'
-
-  def get_absolute_url(self):
-    return reverse('toys_detail', kwargs={'pk': self.id})
 
 
 
